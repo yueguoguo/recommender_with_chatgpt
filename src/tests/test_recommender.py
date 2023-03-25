033f0b567f7a93d9274d949549ec23b58f2a4b9d
yueguoguo <yueguoguo1024@gmail.com>
2023-03-25 08:07:30 +0000

    update test

--- src/tests/test_recommender.py
+++ src/tests/test_recommender.py
@@ -1,15 +1,16 @@
 import pytest
 import pandas as pd
-from main import (
-    download_movielens_data,
-    split_dataframe,
+from recommender import (
+    download_data,
+    split_data,
     train_sar_model,
     evaluate_sar_model,
 )
+from recommenders.models.sar import SAR
 
 
 def test_download_movielens_data():
-    df = download_movielens_data()
+    df = download_data()
     assert isinstance(df, pd.DataFrame)
     assert len(df) == 100000
     assert set(df.columns) == set(["user_id", "item_id", "rating", "timestamp"])
@@ -23,7 +24,7 @@ def test_split_dataframe():
             "rating": [3, 4, 5, 3, 2],
         }
     )
-    train_df, test_df = split_dataframe(df, split_ratio=0.8)
+    train_df, test_df = split_data(df, split_ratio=0.8)
     assert isinstance(train_df, pd.DataFrame)
     assert isinstance(test_df, pd.DataFrame)
     assert len(train_df) + len(test_df) == len(df)
@@ -40,7 +41,7 @@ def test_train_sar_model():
     )
     test_df = pd.DataFrame({"user_id": [1, 2, 3], "item_id": [40, 50, 60]})
     model = train_sar_model(train_df, test_df)
-    assert isinstance(model, SARSingleNode)
+    assert isinstance(model, SAR)
 
 
 def test_evaluate_sar_model():
@@ -52,8 +53,8 @@ def test_evaluate_sar_model():
         }
     )
     test_df = pd.DataFrame({"user_id": [1, 2, 3], "item_id": [40, 50, 60]})
-    model = train_sar_model(train_df, test_df)
-    precision, recall = evaluate_sar_model(model, test_df)
+    model = train_model(train_df)
+    precision, recall = evaluate_model(model, test_df, 10)
     assert isinstance(precision, float)
     assert isinstance(recall, float)
     assert 0.0 <= precision <= 1.0
